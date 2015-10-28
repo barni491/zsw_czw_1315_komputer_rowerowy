0a3b8596656957d253ed7c37e5b4917a2a9c92c8
Piotr Czarny <czarny-piotr@wp.pl>
2015-10-29 00:28:49 +0100

    Fixed models

---
+++ ServerApp/src/main/java/bike/model/Training.java
@@ -0,0 +1,97 @@
+package bike.model;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TRAINING")
+public class Training {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private Timestamp date;
+	private float avgSpeed;
+	private float totalDistance;
+	private float duration;
+	private int climb;
+	private int downhill;
+	private float avgBpm;
+	
+	@OneToMany(mappedBy="training")
+	private Set<TrainingPoint> trainingPoints;
+	
+//	Constructors, getters and setters
+	public Training(Timestamp date, float avgSpeed, float totalDistance, float duration, int climb, int downhill,
+			float avgBpm) {
+		super();
+		this.date = date;
+		this.avgSpeed = avgSpeed;
+		this.totalDistance = totalDistance;
+		this.duration = duration;
+		this.climb = climb;
+		this.downhill = downhill;
+		this.avgBpm = avgBpm;
+	}
+	public Training(Timestamp date) {
+		this.date = date;
+	}
+	public Timestamp getDate() {
+		return date;
+	}
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+	public float getAvgSpeed() {
+		return avgSpeed;
+	}
+	public void setAvgSpeed(float avgSpeed) {
+		this.avgSpeed = avgSpeed;
+	}
+	public float getTotalDistance() {
+		return totalDistance;
+	}
+	public void setTotalDistance(float totalDistance) {
+		this.totalDistance = totalDistance;
+	}
+	public float getDuration() {
+		return duration;
+	}
+	public void setDuration(float duration) {
+		this.duration = duration;
+	}
+	public int getClimb() {
+		return climb;
+	}
+	public void setClimb(int climb) {
+		this.climb = climb;
+	}
+	public int getDownhill() {
+		return downhill;
+	}
+	public void setDownhill(int downhill) {
+		this.downhill = downhill;
+	}
+	public float getAvgBpm() {
+		return avgBpm;
+	}
+	public void setAvgBpm(float avgBpm) {
+		this.avgBpm = avgBpm;
+	}
+	public Set<TrainingPoint> getTrainingPoints() {
+		return trainingPoints;
+	}
+	public void setTrainingPoints(Set<TrainingPoint> trainingPoints) {
+		this.trainingPoints = trainingPoints;
+	}
+	public long getId() {
+		return id;
+	}	
+}
